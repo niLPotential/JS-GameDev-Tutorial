956ccd0847c0c51860c7dad3f31faddc73958eb1
niLPotential <niLPotential@proton.me>
2023-02-23 15:30:13 +0900

    Add Reusable collision detection method

--- script.js
+++ script.js
@@ -52,6 +52,13 @@ window.addEventListener("load", function () {
       }
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
+
+      // collisions with obstacles
+      this.game.obstacles.forEach((obstacle) => {
+        if (this.game.checkCollision(this, obstacle)) {
+          console.log("collision");
+        }
+      });
     }
   }
 
@@ -137,6 +144,13 @@ window.addEventListener("load", function () {
       this.player.update();
       this.obstacles.forEach((obstacle) => obstacle.draw(context));
     }
+    checkCollision(a, b) {
+      const dx = a.collisionX - b.collisionX;
+      const dy = a.collisionY - b.collisionY;
+      const distance = Math.hypot(dy, dx);
+      const sumOfRadii = a.collisionRadius + b.collisionRadius;
+      return (distance < sumOfRadii);
+    }
     init() {
       let attempts = 0;
       while (this.obstacles.length < this.numberOfObstacles && attempts < 500) {
@@ -169,7 +183,6 @@ window.addEventListener("load", function () {
 
   const game = new Game(canvas);
   game.init();
-  console.log(game);
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
