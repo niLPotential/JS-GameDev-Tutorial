f5b5d0a47f06ec5eb7f2537cb9414c552237f0d6
niLPotential <niLPotential@proton.me>
2023-02-24 12:42:49 +0900

    Add  Egg physics

--- script.js
+++ script.js
@@ -179,8 +179,8 @@ window.addEventListener("load", function () {
       this.spriteHeight = 135;
       this.width = this.spriteWidth;
       this.height = this.spriteHeight;
-      this.spriteX = this.collisionX - this.width * 0.5;
-      this.spriteY = this.collisionY - this.height * 0.5 - 30;
+      this.spriteX;
+      this.spriteY;
     }
     draw(context) {
       context.drawImage(
@@ -203,6 +203,24 @@ window.addEventListener("load", function () {
         context.restore();
         context.stroke();
       }
+    }
+    update() {
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5 - 30;
+      let collisionObjects = [this.game.player, ...this.game.obstacles];
+      collisionObjects.forEach((object) => {
+        let [collision, distance, sumOfRadii, dx, dy] = this.game
+          .checkCollision(
+            this,
+            object,
+          );
+        if (collision) {
+          const unit_x = dx / distance;
+          const unit_y = dy / distance;
+          this.collisionX = object.collisionX + (sumOfRadii + 1) * unit_x;
+          this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
+        }
+      });
     }
   }
 
@@ -255,7 +273,10 @@ window.addEventListener("load", function () {
         // animate next frame
         context.clearRect(0, 0, this.width, this.height);
         this.obstacles.forEach((obstacle) => obstacle.draw(context));
-        this.eggs.forEach((egg) => egg.draw(context));
+        this.eggs.forEach((egg) => {
+          egg.draw(context);
+          egg.update();
+        });
         this.player.draw(context);
         this.player.update();
         this.timer = 0;
@@ -266,7 +287,6 @@ window.addEventListener("load", function () {
       if (this.eggTimer > this.eggInterval && this.eggs.length < this.maxEggs) {
         this.addEgg();
         this.eggTimer = 0;
-        console.log(this.eggs);
       } else {
         this.eggTimer += deltaTime;
       }
