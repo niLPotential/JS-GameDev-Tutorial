6b3167e587a6ce46de8ba887a3374b7e290d6cf3
niLPotential <niLPotential@proton.me>
2023-02-24 14:57:46 +0900

    Add Enemy class

--- script.js
+++ script.js
@@ -208,7 +208,11 @@ window.addEventListener("load", function () {
     update() {
       this.spriteX = this.collisionX - this.width * 0.5;
       this.spriteY = this.collisionY - this.height * 0.5 - 30;
-      let collisionObjects = [this.game.player, ...this.game.obstacles];
+      let collisionObjects = [
+        this.game.player,
+        ...this.game.obstacles,
+        ...this.game.enemies,
+      ];
       collisionObjects.forEach((object) => {
         let [collision, distance, sumOfRadii, dx, dy] = this.game
           .checkCollision(
@@ -238,6 +242,67 @@ window.addEventListener("load", function () {
     }
   }
 
+  class Enemy {
+    constructor(game) {
+      this.game = game;
+      this.collisionRadius = 30;
+      this.speedX = Math.random() * 3 + 0.5;
+      this.image = document.getElementById("toad");
+      this.spriteWidth = 140;
+      this.spriteHeight = 260;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.collisionX = this.game.width;
+      this.collisionY = this.game.topMargin +
+        (Math.random() * (this.game.height - this.game.topMargin));
+      this.spriteX;
+      this.spriteY;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.spriteX, this.spriteY);
+      if (this.game.debug) {
+        context.beginPath();
+        context.arc(
+          this.collisionX,
+          this.collisionY,
+          this.collisionRadius,
+          0,
+          Math.PI * 2,
+        );
+        context.save();
+        context.globalAlpha = 0.5;
+        context.fill();
+        context.restore();
+        context.stroke();
+      }
+    }
+    update() {
+      this.collisionX -= this.speedX;
+      if (this.spriteX + this.width < 0) {
+        this.collisionX = this.game.width + this.width +
+          Math.random() * this.game.width * 0.5;
+        this.collisionY = this.game.topMargin +
+          (Math.random() * (this.game.height - this.game.topMargin));
+      }
+      let collisionObjects = [this.game.player, ...this.game.obstacles];
+      collisionObjects.forEach((object) => {
+        let [collision, distance, sumOfRadii, dx, dy] = this.game
+          .checkCollision(
+            this,
+            object,
+          );
+        if (collision) {
+          const unit_x = dx / distance;
+          const unit_y = dy / distance;
+          this.collisionX = object.collisionX + (sumOfRadii + 1) * unit_x;
+          this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
+        }
+      });
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height + 40;
+    }
+  }
+
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
@@ -255,6 +320,7 @@ window.addEventListener("load", function () {
       this.maxEggs = 10;
       this.obstacles = [];
       this.eggs = [];
+      this.enemies = [];
       this.gameObjects = [];
       this.mouse = {
         x: this.width * 0.5,
@@ -287,7 +353,12 @@ window.addEventListener("load", function () {
       if (this.timer > this.interval) {
         // animate next frame
         context.clearRect(0, 0, this.width, this.height);
-        this.gameObjects = [...this.eggs, ...this.obstacles, this.player];
+        this.gameObjects = [
+          ...this.eggs,
+          ...this.obstacles,
+          ...this.enemies,
+          this.player,
+        ];
         // sort by vertical position
         this.gameObjects.sort((a, b) => {
           return a.collisionY - b.collisionY;
@@ -319,7 +390,13 @@ window.addEventListener("load", function () {
     addEgg() {
       this.eggs.push(new Egg(this));
     }
+    addEnemy() {
+      this.enemies.push(new Enemy(this));
+    }
     init() {
+      for (let i = 0; i < 3; i++) {
+        this.addEnemy();
+      }
       let attempts = 0;
       while (this.obstacles.length < this.numberOfObstacles && attempts < 500) {
         let testObstacle = new Obstacle(this);
