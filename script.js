06494db0f05447e6d5936268d9609a119cf1ec85
niLPotential <niLPotential@proton.me>
2023-02-25 14:33:01 +0900

    Add Win and lose condition

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function () {
   ctx.fillStyle = "white";
   ctx.lineWidth = 3;
   ctx.strokeStyle = "black";
-  ctx.font = "40px Helvetica";
+  ctx.font = "40px Bangers";
   ctx.textAlign = "center";
 
   class Player {
@@ -320,7 +320,7 @@ window.addEventListener("load", function () {
       if (this.collisionY < this.game.topMargin) {
         this.markedForDeletion = true;
         this.game.removeGameObjects();
-        this.game.score++;
+        if (!this.game.gameOver) this.game.score++;
         for (let i = 0; i < 3; i++) {
           this.game.particles.push(
             new Firefly(this.game, this.collisionX, this.collisionY, "yellow"),
@@ -416,7 +416,7 @@ window.addEventListener("load", function () {
     }
     update() {
       this.collisionX -= this.speedX;
-      if (this.spriteX + this.width < 0) {
+      if (this.spriteX + this.width < 0 && !this.game.gameOver) {
         this.collisionX = this.game.width + this.width +
           Math.random() * this.game.width * 0.5;
         this.collisionY = this.game.topMargin +
@@ -519,6 +519,8 @@ window.addEventListener("load", function () {
       this.particles = [];
       this.gameObjects = [];
       this.score = 0;
+      this.winningScore = 5;
+      this.gameOver = false;
       this.lostHatchlings = 0;
       this.mouse = {
         x: this.width * 0.5,
@@ -573,7 +575,10 @@ window.addEventListener("load", function () {
       this.timer += deltaTime;
 
       // add eggs periodically
-      if (this.eggTimer > this.eggInterval && this.eggs.length < this.maxEggs) {
+      if (
+        this.eggTimer > this.eggInterval && this.eggs.length < this.maxEggs &&
+        !this.gameOver
+      ) {
         this.addEgg();
         this.eggTimer = 0;
       } else {
@@ -588,6 +593,42 @@ window.addEventListener("load", function () {
         context.fillText("Lost: " + this.lostHatchlings, 25, 100);
       }
       context.restore();
+
+      // win / lose message
+      if (this.score >= this.winningScore) {
+        this.gameOver = true;
+        context.save();
+        context.fillStyle = "rgba(0,0,0,0.5)";
+        context.fillRect(0, 0, this.width, this.height);
+        context.fillStyle = "white";
+        context.textAlign = "center";
+        context.shadowOffsetX = 4;
+        context.shadowOffsetY = 4;
+        context.shadowColor = "black";
+        let message1;
+        let message2;
+        if (this.lostHatchlings <= 5) {
+          // win
+          message1 = "Bullseye!!!";
+          message2 = "You bullied the bullies!";
+        } else {
+          //lose
+          message1 = "Bullocks!";
+          message2 = "You lost" + this.lostHatchlings +
+            "hatchlings, don't be a pushover!";
+        }
+        context.font = "130px Bangers";
+        context.fillText(message1, this.width * 0.5, this.height * 0.5 - 20);
+        context.font = "40px Bangers";
+        context.fillText(message2, this.width * 0.5, this.height * 0.5 + 30);
+        context.fillText(
+          "Final Score " + this.score + ". Press 'R' to butt heads again!",
+          this.width * 0.5,
+          this.height * 0.5 + 80,
+        );
+
+        context.restore();
+      }
     }
 
     checkCollision(a, b) {
