725caf55c0f5a8ba41bc211944c6c10af12cde19
niLPotential <niLPotential@proton.me>
2023-02-25 14:44:35 +0900

    Add Restart game

--- script.js
+++ script.js
@@ -31,6 +31,12 @@ window.addEventListener("load", function () {
       this.frameY = 0;
       this.image = document.getElementById("bull");
     }
+    restart() {
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5 - 100;
+    }
     draw(context) {
       context.drawImage(
         this.image,
@@ -314,7 +320,7 @@ window.addEventListener("load", function () {
     update() {
       this.collisionY -= this.speedY;
       this.spriteX = this.collisionX - this.width * 0.5;
-      this.spriteY = this.collisionY - this.height * 0.5 - 50;
+      this.spriteY = this.collisionY - this.height * 0.5 - 40;
 
       // move to safety
       if (this.collisionY < this.game.topMargin) {
@@ -332,6 +338,7 @@ window.addEventListener("load", function () {
       let collisionObjects = [
         this.game.player,
         ...this.game.obstacles,
+        ...this.game.eggs,
       ];
       collisionObjects.forEach((object) => {
         let [collision, distance, sumOfRadii, dx, dy] = this.game
@@ -349,7 +356,7 @@ window.addEventListener("load", function () {
 
       // collision with enemies
       this.game.enemies.forEach((enemy) => {
-        if (this.game.checkCollision(this, enemy)[0]) {
+        if (this.game.checkCollision(this, enemy)[0] && !this.game.gameOver) {
           this.markedForDeletion = true;
           this.game.removeGameObjects();
           this.game.lostHatchlings++;
@@ -519,7 +526,7 @@ window.addEventListener("load", function () {
       this.particles = [];
       this.gameObjects = [];
       this.score = 0;
-      this.winningScore = 5;
+      this.winningScore = 30;
       this.gameOver = false;
       this.lostHatchlings = 0;
       this.mouse = {
@@ -547,6 +554,7 @@ window.addEventListener("load", function () {
       });
       window.addEventListener("keydown", (e) => {
         if (e.key == "d") this.debug = !this.debug;
+        else if (e.key == "r") this.restart();
       });
     }
     render(context, deltaTime) {
@@ -653,6 +661,24 @@ window.addEventListener("load", function () {
         !object.markedForDeletion
       );
     }
+    restart() {
+      this.player.restart();
+      this.obstacles = [];
+      this.eggs = [];
+      this.enemies = [];
+      this.hatchlings = [];
+      this.particles = [];
+      this.mouse = {
+        x: this.width * 0.5,
+        y: this.height * 0.5,
+        pressed: false,
+      };
+      this.score = 0;
+      this.lostHatchlings = 0;
+      this.gameOver = false;
+      this.init();
+    }
+
     init() {
       for (let i = 0; i < 5; i++) {
         this.addEnemy();
